7549cf43ec9918e706d9c3d62bc15093fa6923aa
Beichen Chen <beichen@kth.se>
2017-02-15 20:15:26 +0100

    change page name to index.html

--- js/view/viewController.js
+++ js/view/viewController.js
@@ -15,11 +15,11 @@ var viewController = function(model, indexView, sideBarView, selectDishView, dis
   })
 
   selectDishView.dishesPics.on("click", "img", function() {
-    document.getElementById("selectDishView").style.display = "none";
+    selectDishView.container[0].style.display = "none";
     var dishID = $(this).attr('dishID');
     dishView.dishID = dishID;
     dishView.buildPage();
-    document.getElementById("dishView").style.display = "block";
+    dishView.container[0].style.display = "block";
   });
 
   dishView.backButton.click(function() {
